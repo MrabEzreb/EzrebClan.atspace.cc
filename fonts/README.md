f27d5ac97b200a862b027f35cd5b2ad092ccb683
MrabEzreb <de013b3f@opayq.com>
2015-02-12 12:26:16 -0500

    I think im done with this for today :P

--- fonts/README.md
+++
@@ -1,4 +0,0 @@
-MrabEzreb.github.io
-===================
-
-This should work to do what I want. Basically, you should be able to calculate the delta-v of various maneuvers in kerbal space program.
--- fonts/javascripts/announcement.js
+++
@@ -1,9 +0,0 @@
-function setAnnouncement() {
-    "use strict";
-    document.getElementById("announcement").innerHTML = "Test Announcement";
-    document.getElementById("announcement").style.display = "in-line";
-    document.getElementById("announcement").style.fontSize = "25px";
-    document.getElementById("announcement").style.backgroundColor = "green";
-    document.getElementById("announcement").style.color = "white";
-}
-window.onload = setAnnouncement;
