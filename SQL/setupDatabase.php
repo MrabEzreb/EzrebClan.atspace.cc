1f08f71d4c219a0ab1e34e7503fbf8a1567a76c6
MrabEzreb <de013b3f@opayq.com>
2015-02-18 11:04:08 -0500

    Alright, added a php file that will setup the databases :D

---
+++ SQL/setupDatabase.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $servername = "mysql14.000webhost.com";
+            $username = "a9340268_test";
+            $password = "4edxz7yhbn";
+            $db = "a9340268_test";
+            try {
+                $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                }
+            catch(PDOException $e)
+                {
+                echo "Connection failed: " . $e->getMessage();
+                }
+            $stmt = $conn->prepare("CREATE TABLE `Profiles` (
+  `name` text COLLATE latin1_general_ci NOT NULL,
+  `email` text COLLATE latin1_general_ci NOT NULL,
+  `username` text COLLATE latin1_general_ci NOT NULL,
+  `password` text COLLATE latin1_general_ci NOT NULL,
+  `isActive` tinyint(1) NOT NULL,
+  `hash` text COLLATE latin1_general_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;");
+            $conn = null;
+        ?>
+    </body>
+</html>
